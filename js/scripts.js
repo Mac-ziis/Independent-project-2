67658a3a777eee7827c06e2376e3a57f1b82af36
Mac-ziis <macg286@gmail.com>
2023-06-15 16:28:03 -0700

    Add more to the javascript WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,34 @@
-const init = function() {
-  document.getElementById("btn-submit").addEventListener("click", submit);
-  document.getElementById("btn-cancel").addEventListener("click", cancel);
-}
+// const init = function() {
+//   document.getElementById("btn-submit").addEventListener("click", submit);
+//   document.getElementById("btn-cancel").addEventListener("click", cancel);
+// }
 
-const cancel = function(ev){
-  document.getElementById("dropdown").reset;
-  ev.preventDefault();
-}
+// const cancel = function(ev){
+//   document.getElementById("dropdown").reset;
+//   ev.preventDefault();
+// }
 
 const submit = function(ev) {
   ev.preventDefault();
 }
 
-// function return() {
+function selected(event) {
+  event.preventDefault();
+  let selection = document.getElementById("dropdown").value;
+}
+
+window.addEventListener("load", function() {
+  document.getElementById("environment", "kind", "naturally").addEventListen("submit", selected);
+});
+
+  const 
   if (document.getElementById("dropdown1").value === "1" && document.getElementById("dropdown2").value === "1" && document.getElementById("dropdown3").value === "1") {
     document.getElementById("javascript").removeAttribute("class", "hidden");
   }
-// }
 
-document.addEventListener("DOMContentLoaded", init);
+
+  // function selectValues() {
+  //   document.querySelector("#dropdown");
+  //   output = select.options[select.selectedIndex].value;
+  //   document.querySelector("#javascript").textContent = javascript;
+  // }
