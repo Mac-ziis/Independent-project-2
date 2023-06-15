9e1dcd1868acefd29083c740a38e24a43e54d458
Mac-ziis <macg286@gmail.com>
2023-06-15 15:39:06 -0700

    Add if statements for the separate options in the drop down menu WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
 const init = function() {
-  document.getElementById("btn").addEventListener("click", submit);
-  document.getElementById("btn").addEventListener("click", cancel);
+  document.getElementById("btn-submit").addEventListener("click", submit);
+  document.getElementById("btn-cancel").addEventListener("click", cancel);
 }
 
 const cancel = function(ev){
+  document.getElementById("dropdown").reset;
   ev.preventDefault();
-  document.getElementById("dropdown").reset();
 }
 
 const submit = function(ev) {
   ev.preventDefault();
-  
 }
+
+// function return() {
+  if (document.getElementById("dropdown1").value === "1" && document.getElementById("dropdown2").value === "1" && document.getElementById("dropdown3").value === "1") {
+    document.getElementById("javascript").removeAttribute("class", "hidden");
+  }
+// }
 
 document.addEventListener("DOMContentLoaded", init);
