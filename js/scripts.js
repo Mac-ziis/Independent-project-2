7722b07872f736a697418a7fe05970018b9cf85a
Mac-ziis <macg286@gmail.com>
2023-06-15 13:45:42 -0700

    Add the submit button to index.html and the styling in styles.css as well as framework for js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 window.onload = function() {
-  const form = document.getElementById('language-selector');
+  const form = document.getElementById('language-selector').value;
   form.onsubmit = function (event) {
     event.preventDefault();
   }
 
+  document.querySelector()
+  document.querySelector("div#recommendation").removeAttribute("class");
 
 }
