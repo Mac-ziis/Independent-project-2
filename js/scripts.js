78233378a3ed4e984d4ba10df830a7535a073685
Mac-ziis <macg286@gmail.com>
2023-06-16 14:04:20 -0700

    Add a function to keep more than 1 response being retrieved at a time until the page is reloaded

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 
 document.addEventListener("DOMContentLoaded", () => {
+  let responseDisplayed = false;
+
   const visibilityAccessor = () => {
+    if (responseDisplayed) {
+      return;
+    }
+
     let value1 = document.getElementById("dropdown1-select").value === "1";
     let value2 = document.getElementById("dropdown2-select").value === "1";
     let value3 = document.getElementById("dropdown3-select").value === "1";
@@ -13,40 +19,76 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (value1 && value2 && value3) {
       document.getElementById("recommendation1").removeAttribute("class", "hidden");
+      responseDisplayed = true;
     } else if (value4 && value2 && value3) {
       document.getElementById("recommendation2").removeAttribute("class", "hidden");
+      responseDisplayed = true;
     } else if (value7 && value2 && value3) {
       document.getElementById("recommendation3").removeAttribute("class", "hidden");
+      responseDisplayed = true;
     } else if (value1 && value2 && value6) {
       document.getElementById("recommendation2").removeAttribute("class", "hidden");
+      responseDisplayed = true;
     } else if (value1 && value2 && value9) {
       document.getElementById("recommendation2").removeAttribute("class", "hidden");
+      responseDisplayed = true;
     } else if (value1 && value5 && value6) {
       document.getElementById("recommendation3").removeAttribute("class", "hidden");
+      responseDisplayed = true;
     } else if (value1 && value5 && value9) {
       document.getElementById("recommendation2").removeAttribute("class", "hidden");
+      responseDisplayed = true;
     } else if (value1 && value8 && value6) {
       document.getElementById("recommendation3").removeAttribute("class", "hidden");
+      responseDisplayed = true;
     } else if (value1 && value8 && value9) {
       document.getElementById("recommendation1").removeAttribute("class", "hidden");
+      responseDisplayed = true;
     } else if (value7 && value8 && value9) {
       document.getElementById("recommendation1").removeAttribute("class", "hidden");
+      responseDisplayed = true;
     } else if (value4 && value5 && value6) {
       document.getElementById("recommendation2").removeAttribute("class", "hidden");
+      responseDisplayed = true;
     } else if (value4 && value5 && value9) {
       document.getElementById("recommendation3").removeAttribute("class", "hidden");
+      responseDisplayed = true;
     } else if (value4 && value8 && value9) {
       document.getElementById("recommendation2").removeAttribute("class", "hidden");
+      responseDisplayed = true;
     } else if (value4 && value2 && value6) {
       document.getElementById("recommendation1").removeAttribute("class", "hidden");
+      responseDisplayed = true;
     } else if (value4 && value2 && value9) {
       document.getElementById("recommendation3").removeAttribute("class", "hidden");
+      responseDisplayed = true;
     } else if (value7 && value2 && value6) {
       document.getElementById("recommendation2").removeAttribute("class", "hidden");
+      responseDisplayed = true;
     } else if (value7 && value2 && value9) {
       document.getElementById("recommendation1").removeAttribute("class", "hidden");
+      responseDisplayed = true;
+    } else if (value4 && value8 && value6) {
+      document.getElementById("recommendation3").removeAttribute("class", "hidden");
+      responseDisplayed = true;
+    } else if (value7 && value5 && value9) {
+      document.getElementById("recommendation2").removeAttribute("class", "hidden");
+      responseDisplayed = true;
+    } else if (value7 && value5 && value6) {
+      document.getElementById("recommendation1").removeAttribute("class", "hidden");
+      responseDisplayed = true;
+    } else if (value7 && value2 && value9) {
+      document.getElementById("recommendation1").removeAttribute("class", "hidden");
+      responseDisplayed = true;
+    } else if (value7 && value5 && value6) {
+      document.getElementById("recommendation3").removeAttribute("class", "hidden");
+      responseDisplayed = true;
+    } else if (value1 && value5 && value3) {
+      document.getElementById("recommendation2").removeAttribute("class", "hidden");
+      responseDisplayed = true;
     } else {
       document.getElementById("recommendation4").removeAttribute("class", "hidden");
+      responseDisplayed = true;
     }
   };
 
