540d55409be7c7c36868d26a63c02e43d8365ea1
Mac-ziis <macg286@gmail.com>
2023-06-16 10:09:02 -0700

    Add function declaration for dropdown values and the WIP else if

--- js/scripts.js
+++ js/scripts.js
@@ -18,13 +18,36 @@ function selected(event) {
 }
 
 window.addEventListener("load", function() {
-  document.getElementById("environment", "kind", "naturally").addEventListen("submit", selected);
+  document.getElementById("environment", "kind", "naturally").addEventListener("submit", selected);
 });
 
-  const 
-  if (document.getElementById("dropdown1").value === "1" && document.getElementById("dropdown2").value === "1" && document.getElementById("dropdown3").value === "1") {
-    document.getElementById("javascript").removeAttribute("class", "hidden");
+  const choices = function() {
+    let value = document.getElementById("dropdown1").value === "1";
+    let value2 = document.getElementById("dropdown2").value === "1";
+    let value3 = document.getElementById("dropdown3").value === "1";
+    let value4 = document.getElementById("dropdown1").value === "2";
+    let value5 = document.getElementById("dropdown2").value === "2";
+    let value6 = document.getElementById("dropdown3").value === "2";
+    let value7 = document.getElementById("dropdown1").value === "3";
+    let value8 = document.getElementById("dropdown2").value === "3";
+    let value9 = document.getElementById("dropdown3").value === "3";
+
+    if (value && value2 && value3) {
+      document.getElementById("js").removeAttribute("class", "hidden");
+    } else if (value4 && value2 && value3) {
+      document.getElementById("c-sharp").removeAttribute("class", "hidden");
+    }
+
+  const visibilityAccessor = () => {
+
   }
+}
+
+// <html> 
+//   <option>
+//     <div onclick={{visibilityAccessor()}}></div> </div>
+//   </option>
+// </html>
 
 
   // function selectValues() {
